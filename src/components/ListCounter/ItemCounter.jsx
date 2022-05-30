ec420d6dfd20c5b289e1a77f34036383d53a8899
Nicolasd Diaz <nicolasdiazoff@gmail.com>
2022-05-30 10:31:35 -0500

    change styles of ItemCounter

--- src/components/ListCounter/ItemCounter.jsx
+++ src/components/ListCounter/ItemCounter.jsx
@@ -1,80 +1,82 @@
 import React, { useContext } from 'react';
-import { createUseStyles } from 'react-jss';
+import styled from 'styled-components';
 import { CounterContext } from '../../context/counterContext';
+
+const StyledItem = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-content: space-between;
+	align-items: center;
+	width: 90%;
+	margin: auto;
+	box-sizing: border-box;
+	padding: 20px;
+	border-radius: 5px;
+	border: 1px solid #dadefb;
+`;
+
+const StyledItemBox = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const StyledItemBoxButton = styled(StyledItemBox)`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
+
+const StyledItemCount = styled.h1`
+	font-size: 2.2rem;
+	font-weight: 800;
+	margin: 0;
+	color: #000;
+`;
+
+const StyledItemTitle = styled.p`
+	font-size: 1rem;
+	font-weight: 600;
+	margin: 0;
+	color: #4f4f4f;
+`;
+
+const StyledButton = styled.button`
+	background-color: #dadefb;
+	width: 50px;
+	height: 50px;
+	border-radius: 50px;
+	border: none;
+	color: #000;
+	outline: none;
+	font-size: 2rem;
+	font-weight: 600;
+	margin: 0;
+	margin-left: 5px;
+	margin-right: 5px;
+`;
 
 function ItemCounter({ id, title, count }) {
 	const { dispatch } = useContext(CounterContext);
-	const classes = useStyles();
 	return (
-		<>
-			<div className={classes.ItemCounter}>
-				<div className={classes.ItemCounterInfo}>
-					<h1>{count}</h1>
-					<p>{title}</p>
-				</div>
-				<div className={classes.ItemCounterBox}>
-					<button
-						onClick={() => dispatch({ type: 'increment', id: id })}>
-						+
-					</button>
-					<button
-						onClick={() => dispatch({ type: 'decrement', id: id })}>
-						-
-					</button>
-				</div>
-			</div>
-		</>
+		<StyledItem>
+			<StyledItemBox>
+				<StyledItemCount>{count}</StyledItemCount>
+				<StyledItemTitle>{title}</StyledItemTitle>
+			</StyledItemBox>
+			<StyledItemBoxButton>
+				<StyledButton
+					onClick={() => dispatch({ type: 'increment', id: id })}
+					label='+'>
+					+
+				</StyledButton>
+				<StyledButton
+					onClick={() => dispatch({ type: 'decrement', id: id })}>
+					-
+				</StyledButton>
+			</StyledItemBoxButton>
+		</StyledItem>
 	);
 }
-
-const useStyles = createUseStyles({
-	ItemCounter: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignContent: 'space-between',
-		alignItems: 'center',
-		width: '90%',
-		margin: 'auto',
-		boxSizing: 'border-box',
-		padding: 20,
-		borderRadius: 5,
-		border: '1px solid #DADEFB',
-	},
-	ItemCounterInfo: {
-		width: '100%',
-		'& h1': {
-			fontSize: '2.2rem',
-			fontWeight: 800,
-			margin: 0,
-			color: '#000',
-		},
-		'& p': {
-			fontSize: '1rem',
-			fontWeight: 600,
-			margin: 0,
-			color: '#4f4f4f',
-		},
-	},
-	ItemCounterBox: {
-		width: '100%',
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'flex-end',
-		'& button': {
-			backgroundColor: '#DADEFB',
-			width: 50,
-			height: 50,
-			borderRadius: 50,
-			border: 'none',
-			color: '#000',
-			outline: 'none',
-			fontSize: '2rem',
-			fontWeight: 600,
-			margin: 0,
-			marginLeft: 5,
-			marginRight: 5,
-		},
-	},
-});
 
 export default ItemCounter;
