85b5be82eedee6b81ab05197a16f6719f38a66f0
Nicolasd Diaz <nicolasdiazoff@gmail.com>
2022-05-30 10:31:40 -0500

    add theme

---
+++ src/config/theme.js
@@ -0,0 +1,48 @@
+const theme = {
+	colors: {
+		darkBlue: '#002033',
+		grayBlue: '#33617D',
+		darkGray: '#667985',
+		gray4: '#B6C9D4',
+		gray3: '#CCD9E0',
+		gray2: '#DEE4E8',
+		gray1: '#F2F6F8',
+		white: '#FFFFFF',
+		blue: '#0078D2',
+		softBlue: '#48A9F5',
+		green: '#24C38E',
+		orange: '#F59B68',
+		red: '#F85E5D',
+		yellow: '#F2C94C',
+	},
+	fontSize: {
+		size10: '10px',
+		size12: '12px',
+		size14: '14px',
+		size16: '16px',
+		size18: '18px',
+		size20: '20px',
+		size22: '22px',
+		size24: '24px',
+		size28: '28px',
+	},
+	fontWeight: {
+		light: 300,
+		regular: 400,
+		medium: 500,
+		semiBold: 600,
+		bold: 700,
+		extraBold: 800,
+	},
+	lineHeights: ['16px'],
+	breakpoints: {
+		xs: '768px',
+		sm: '992px',
+		md: '1200',
+	},
+	transition: {
+		default: 'all ease 0.2s',
+	},
+};
+
+export default theme;
